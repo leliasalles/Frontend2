b38d8c0f7508920b653fb48842183976547d6d46
Lélia Salles <89154346+leliasalles@users.noreply.github.com>
2021-10-25 20:58:09 -0300

    subiu arquivos

---
+++ Aula05/Mesa/js/script.js
@@ -0,0 +1,8 @@
+
+document.querySelector("button").addEventListener("click", function() {
+  document.body.style.backgroundColor = "black";
+  });
+
+  document.querySelector("h1").addEventListener("click", function() {
+    document.body.style.color = "white";
+    });
